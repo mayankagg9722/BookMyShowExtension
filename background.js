0c4b083d09a86b79290334b7b4351dcc6b750342
Mayank Aggarwal <mayaggar@microsoft.com>
2019-07-11 17:08:54 +0530

    integrated apis

--- background.js
+++ background.js
@@ -4,14 +4,53 @@
 
 'use strict';
 
-// function getBgColors (tab) {
-//   console.log(document.body)
-// }
 
-// chrome.browserAction.onClicked.addListener(getBgColors);
-
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    console.log(tabs)
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    if (request.message === "mydestinaitonresposnse"){
+      var api_key = "&key=AIzaSyCczSkhnEBtPw3A9VJ-gRSNARuSyjQxXvo";
+      var distanceCalculationURL = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+request.myAddress+"&destinations="+request.destinationConcatenation+api_key
+      distanceCalculationURL = distanceCalculationURL.replace(/,/g, '')
+      distanceCalculationURL = distanceCalculationURL.replace(/\s/g, '+')
+      // logging(distanceCalculationURL)
+      httpGetAsync(distanceCalculationURL,function(distanceResponse){
+        // logging(distanceResponse)
+        sendResponse({message: distanceResponse});
+        sendCopyToContentScript(distanceResponse)
+      })
+      // var myurl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626"+api_key      
+      // httpGetAsync(myurl,function(res){
+      //   // console.log(res)
+      //   sendResponse({message: res});
+      //   sendCopyToContentScript(res)
+      // })
+    }
   });
-});
+
+// chrome.browserAction.onClicked.addListener(function() {
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     console.log(tabs)
+//   });
+// });
+
+function httpGetAsync(theUrl, callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); 
+  xmlHttp.send(null);
+}
+
+function sendCopyToContentScript(toBeCopied){
+  chrome.tabs.getSelected(null, function(tab) {
+    var joinedMessage = toBeCopied; 
+    chrome.tabs.sendMessage(tab.id, {message: joinedMessage});
+   });
+}
+
+function logging(logg){
+  console.log(logg);
+}
--- options.js
+++ options.js
@@ -4,91 +4,70 @@
 
 'use strict';
 
+var api_key = "&key=AIzaSyCczSkhnEBtPw3A9VJ-gRSNARuSyjQxXvo";
 
 window.onload = function() {
-  // getMylocation();
-  // getTheatres();
+  
+  chrome.runtime.onMessage.addListener(
+    function(request, sender) {
+      console.log(request)
+    });
+
+  getTheatres(function(destinations,totalTheatres){
+    if(totalTheatres==destinations.length){
+      var destinationConcatenation = destinations.join("|")
+      getMylocation(destinationConcatenation);
+    }
+  });
+
 }
 
-function getMylocation(){
+function getMylocation(destinationConcatenation){
   navigator.geolocation.getCurrentPosition(function(position){
-    // console.log("Latitude: " + position.coords.latitude , 
-    // "Longitude: " + position.coords.longitude)
-    getMyaddress(position.coords.latitude,position.coords.longitude)
+    getMyaddress(position.coords.latitude,position.coords.longitude,function(myAddress){
+      chrome.runtime.sendMessage({message: "mydestinaitonresposnse",myAddress:myAddress,destinationConcatenation:destinationConcatenation}, (response) => {
+        console.log(response);
+      });
+    })
+  },function(error){
+    logging(error)
   })
 }
 
-function getMyaddress(lat,lng){
-  var newURL = "https://us1.locationiq.com/v1/reverse.php?key=f178a1530c6123&lat="+lat+"&lon="+lng+"&format=json"
-  httpGetAsync(newURL,function(responeText){
-    var myAddress = JSON.parse(responeText).display_name
-    console.log(myAddress)
+function getMyaddress(lat,lng,callback){
+  var reverseGeocodingURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+api_key
+  httpGetAsync(reverseGeocodingURL,function(responeText){
+    var myAddress = JSON.parse(responeText).results[0].formatted_address
+    callback(myAddress)
   })
 }
-function getTheatres(){
+
+function getTheatres(callback){
   var theatres = document.getElementsByClassName("__venue-name")
+  var destinations = [];
   for(var i=0;i<theatres.length;i++){
     var href = document.getElementsByClassName("__venue-name")[i].href
-    var theatreAddress = httpGetTheatres(href)
-    console.log(theatreAddress)
+    httpGetAsync(href,function(theatreAddressResponse){
+    var content = document.createElement("div");
+    content.innerHTML = theatreAddressResponse
+    var theatreAddress = content.getElementsByTagName("address")[0].children[1].innerText;
+    destinations.push(theatreAddress)
+    callback(destinations,theatres.length)
+    })
   }
 }
 
-function httpGetTheatres(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    var content = document.createElement("div");
-    content.innerHTML = xmlHttp.responseText
-    return content.getElementsByTagName("address")[0].children[1].innerText;
-}
-
-
-
 function httpGetAsync(theUrl, callback)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); 
-    xmlHttp.withCredentials = true
-    xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*")
-    xmlHttp.setRequestHeader("Content-Type", "application/json") 
-    xmlHttp.send(null);
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); 
+  xmlHttp.send(null);
 }
 
-function calculateDistance(lat1, long1, lat2, long2)
-{    
-
-      //radians
-      lat1 = (lat1 * 2.0 * Math.PI) / 60.0 / 360.0;      
-      long1 = (long1 * 2.0 * Math.PI) / 60.0 / 360.0;    
-      lat2 = (lat2 * 2.0 * Math.PI) / 60.0 / 360.0;   
-      long2 = (long2 * 2.0 * Math.PI) / 60.0 / 360.0;       
-
-
-      // use to different earth axis length    
-      var a = 6378137.0;        // Earth Major Axis (WGS84)    
-      var b = 6356752.3142;     // Minor Axis    
-      var f = (a-b) / a;        // "Flattening"    
-      var e = 2.0*f - f*f;      // "Eccentricity"      
-
-      var beta = (a / Math.sqrt( 1.0 - e * Math.sin( lat1 ) * Math.sin( lat1 )));    
-      var cos = Math.cos( lat1 );    
-      var x = beta * cos * Math.cos( long1 );    
-      var y = beta * cos * Math.sin( long1 );    
-      var z = beta * ( 1 - e ) * Math.sin( lat1 );      
-
-      beta = ( a / Math.sqrt( 1.0 -  e * Math.sin( lat2 ) * Math.sin( lat2 )));    
-      cos = Math.cos( lat2 );   
-      x -= (beta * cos * Math.cos( long2 ));    
-      y -= (beta * cos * Math.sin( long2 ));    
-      z -= (beta * (1 - e) * Math.sin( lat2 ));       
-
-      return (Math.sqrt( (x*x) + (y*y) + (z*z) )/1000);  
+function logging(logg){
+  console.log(logg);
 }
-
-
